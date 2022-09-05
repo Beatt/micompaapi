d34508100864ec4fe576adb75d429f545fade344
Gabriel Jiménez <ggabrieljimenezmartinez@gmail.com>
2022-09-05 01:27:58 -0500

    [Feature] catalogs (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :addresses, only: %i[index show create update destroy]
+      get '/catalogs/postal_code', to: 'catalogs#postal_code'
+      get '/catalogs/suburbs', to: 'catalogs#suburbs'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,19 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_044633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "codigo_postals", force: :cascade do |t|
-    t.string "cp"
-    t.string "street"
-    t.string "municipality"
-    t.string "state"
-    t.string "city"
-    t.string "country"
-    t.string "interior_number"
-    t.string "outdoor_number"
-    t.string "suburb"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
